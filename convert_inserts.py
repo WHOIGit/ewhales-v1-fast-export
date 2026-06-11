9666a77e997cd96ddd6bae262d9b7138bfbb134c
Alexander Laties <alex@laties.info>
2026-06-11 10:15:12 -0400

    comment and cleanup converter script

--- convert_inserts.py
+++ convert_inserts.py
@@ -1,9 +1,20 @@
+# convert_inserts.py
+# 
+# Intended to turn a sql dump file of single row inserts into a file of
+# multiple row insert statements of up to the designated batch size.
+# 
+# MySQL has no constraint on how many rows can be inserted with a single
+# insert statement, but it must fit within the configured max_allowed_packet
+# of the server. Batch sizes need to be chosen such that the batched insert
+# statements will fit within. Adjust as needed.
+
 import sys
 
 def convert(input_file, output_file, batch_size=2000):
     batch = []
     current_prefix = None
     
+    # need to use surrogateescape to handle raw bytes in the dump
     with open(input_file, 'r', encoding='utf-8', errors='surrogateescape') as f_in, \
          open(output_file, 'w', encoding='utf-8', errors='surrogateescape') as f_out:
          
@@ -41,7 +52,9 @@ def convert(input_file, output_file, batch_size=2000):
         flush()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python convert_inserts.py <input_file.sql> <output_file.sql>")
-        sys.exit(1)
-    convert(sys.argv[1], sys.argv[2])
+    if len(sys.argv) > 2:
+        infile, outfile = sys.argv[1], sys.argv[2]
+        batch_size = int(sys.argv[3]) if len(sys.argv) > 3 else 2000
+        convert(sys.argv[1], sys.argv[2], batch_size)
+        exit(0)
+    print("Usage: python convert_inserts.py <input_file.sql> <output_file.sql> [<batch_size>]")
